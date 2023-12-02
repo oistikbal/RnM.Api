3d2b56d4cd7dd999663fe024c0d5390a75662332
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2023-12-02 18:52:44 +0300

    Fix Solutions structure

---
+++ Rnm.Api.Test/ApiKey.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using RnM.Api;
+using RnM.Api.Auth;
+
+namespace RnM.Api.Test
+{
+    [TestClass]
+    public class ApiTest
+    {
+
+        //Tried to add test db for the tests but failed, so only apikey check is done.
+
+        [TestMethod]
+        public async Task TestApiKeyMissing()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+
+            var response = await httpClient.GetAsync("");
+            var stringResut = await response.Content.ReadAsStringAsync();
+
+            Assert.AreNotEqual("Api Key Missing", stringResut);
+        }
+
+        [TestMethod]
+        public async Task TestApiKeyInvalid()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+
+            var headers = httpClient.DefaultRequestHeaders;
+            headers.Add(ApiKeyAuthFilter.ApiKeyHeaderName, "dasdasdasd");
+
+            var response = await httpClient.GetAsync("");
+            var stringResut = await response.Content.ReadAsStringAsync();
+
+            Assert.AreNotEqual("Invalid Api Key", stringResut);
+        }
+
+        [TestMethod]
+        public async Task TestApiKeyTrue()
+        {
+            var webAppFactory = new WebApplicationFactory<Program>();
+            var httpClient = webAppFactory.CreateDefaultClient();
+
+            var env = webAppFactory.Services.GetRequiredService<IConfiguration>();
+            var headers = httpClient.DefaultRequestHeaders;
+            headers.Add(ApiKeyAuthFilter.ApiKeyHeaderName, env.GetValue<string>(ApiKeyAuthFilter.ApiKeySectionName));
+
+            var response = await httpClient.GetAsync("/Character");
+            var stringResut = await response.Content.ReadAsStringAsync();
+
+            Assert.AreNotEqual(ApiKeyAuthFilter.KeyInvalid, stringResut);
+            Assert.AreNotEqual(ApiKeyAuthFilter.KeyMissing, stringResut);
+        }
+    }
+}
